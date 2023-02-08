d7fb60797815b4d6ee4837a2699cd33c6a16d129
B-ANTONY-J <antony.jones14@gmail.com>
2023-02-07 23:17:18 -0500

    Add project files.

---
+++ String_Reverse_WO_Function/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace String_Reverse_WO_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Please enter a name you would like to see the reverse order of and to see " +
+                $"whether or not it is a palindrome:");
+            StringReverse(Convert.ToString(Console.ReadLine()));
+
+        }
+        private static void StringReverse(string stringInput)
+        {
+            // Reverse using for loop  
+            //Convert input string to char array and loop through  
+            char[] stringArray = stringInput.ToCharArray();
+
+            string reverse = String.Empty;
+
+
+            for (int i = stringArray.Length - 1; i >= 0; i--)
+            {
+                reverse += stringArray[i];
+            }
+
+            if (stringInput == reverse) 
+                {
+                    Console.WriteLine($"\nThis is a Palindrome\n");
+                }
+            else 
+                {
+                Console.WriteLine($"\nIt's not a Palindrome\n");
+                }
+            Console.WriteLine(reverse);
+
+
+
+        }
+    }
+}
