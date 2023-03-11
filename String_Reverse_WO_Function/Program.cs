eb20a7d826df4b3f62bd94854f35cf1994b32ac7
B-ANTONY-J <antony.jones14@gmail.com>
2023-03-10 22:15:48 -0500

    adding C# Class file that will contatin User input methods and seperate it from the main Program.cs

--- String_Reverse_WO_Function/Program.cs
+++ String_Reverse_WO_Function/Program.cs
@@ -1,36 +1,42 @@
 ﻿namespace String_Reverse_WO_Function
-{
+{   //This code will be restructed to have the UI methods placed in a different file
     internal class Program
     {
         static void Main(string[] args)
+
         {
-
-            Console.WriteLine($"\nPlease enter a name you would like to see the reverse order of and to see " +
-                $"whether or not it is a palindrome: ");
-
+            Console.WriteLine("Lets begin this String Game\n");
+            UIMethods.UserDisplay();
+           /* Console.WriteLine($"\nPlease enter a name you would like to see the reverse order of and to see " +
+                $"whether or not it is a palindrome: ");*/
+           /*
             string stringInputForward = Console.ReadLine().ToLower();
             string reverseLoop = string.Empty;
-
+            */
+            UIMethods.StringLoop();
+            /*
             for (int j = 0; j < stringInputForward.Length; j++)
             {
                 
                 reverseLoop += stringInputForward[stringInputForward.Length -j - 1];
 
-            }
-            Console.WriteLine($"\nThe reverse string of the input string is: ");
+            }*/
+           /* Console.WriteLine($"\nThe reverse string of the input string is: ");
             Console.WriteLine($"{reverseLoop}");
 
-            Console.WriteLine("\n");
-            if (reverseLoop == stringInputForward)
+            Console.WriteLine("\n");*/
+           /* if (reverseLoop == stringInputForward)
             {
                 Console.WriteLine($"\nFinally this reverse string result of {reverseLoop} matches the string input of {stringInputForward} which makes the result a palinDrome!\n");
             }
             else
             {
                 Console.WriteLine($"\nThis reverse string results of {reverseLoop} is not a Palindrome of the input string {stringInputForward}\n");
+            }*/
+
+            if (UIMethods.StringLoop() == true) 
+            {
             }
-
-
         }
 
     }
---
+++ String_Reverse_WO_Function/UIMethods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Reverse_WO_Function
+{
+    public static class UIMethods
+    {
+        public static void UserDisplay()
+        {
+            Console.WriteLine($"\nPlease enter a name you would like to see the reverse order of and to see " +
+                $"whether or not it is a palindrome: ");
+        }
+
+        public static void StringLoop()
+        {
+            string stringInputForward = Console.ReadLine().ToLower();
+            string reverseLoop = string.Empty;
+
+            for (int j = 0; j < stringInputForward.Length; j++)
+            {
+
+                reverseLoop += stringInputForward[stringInputForward.Length - j - 1];
+
+            }
+            Console.WriteLine($"\nThe reverse string of the input string is: ");
+            Console.WriteLine($"{reverseLoop}");
+
+            Console.WriteLine("\n");
+
+            if (reverseLoop == stringInputForward)
+            {
+                Console.WriteLine($"\nFinally this reverse string result of {reverseLoop} matches the string input of {stringInputForward} which makes the result a palinDrome!\n");
+            }
+            else
+            {
+                Console.WriteLine($"\nThis reverse string results of {reverseLoop} is not a Palindrome of the input string {stringInputForward}\n");
+            }
+
+        }
+
+
+    }
+}
+
