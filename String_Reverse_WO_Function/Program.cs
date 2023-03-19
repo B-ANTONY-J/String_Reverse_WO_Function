56369293e104b3b8775879767af87af3f55c760a
B-ANTONY-J <64221875+B-ANTONY-J@users.noreply.github.com>
2023-03-19 16:37:08 -0400

    Replaced "namespace" coding with the "using" statement to allow for the UIMetods.cs will be called from the Program.cs

--- String_Reverse_WO_Function/Program.cs
+++ String_Reverse_WO_Function/Program.cs
@@ -1,20 +1,11 @@
-﻿namespace String_Reverse_WO_Function
-{   //This code will be restructed to have the UI methods placed in a different file
-   
-    internal class Program
-    {
-        static void Main(string[] args)
+﻿using String_Reverse_WO_Function;
+//This code will be restructed to have the UI methods placed in a different file
 
-        {
-            //Method to call userDisplay Console.WrilteLine
-            UIMethods.UserDisplay();
-          
-            //Method called to iterate user input through a for loop and then compares the output to the conditional statements
-            UIMethods.StringInputLoop();
-      
-           //UIMethods.IfElseInput();
-            
-        }
+//Method to call userDisplay Console.WrilteLine
+UIMethods.UserDisplay();
 
-    }
-}
+//Method called to iterate user input through a for loop and then compares the output to the conditional statements
+UIMethods.StringInputLoop();
+
+//UIMethods.IfElseInput();
+
